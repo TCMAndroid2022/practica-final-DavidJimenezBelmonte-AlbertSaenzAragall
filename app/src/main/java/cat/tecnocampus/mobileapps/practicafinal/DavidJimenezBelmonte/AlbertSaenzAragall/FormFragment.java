52210ffe562a8798c590fd1ca93f72791ffdd9af
David Jimenez Belmonte <d.jimenez.belmonte@gmail.com>
2022-06-25 17:01:05 +0200

    btn lleva a fragment 2

--- app/src/main/java/cat/tecnocampus/mobileapps/practicafinal/DavidJimenezBelmonte/AlbertSaenzAragall/FormFragment.java
+++ app/src/main/java/cat/tecnocampus/mobileapps/practicafinal/DavidJimenezBelmonte/AlbertSaenzAragall/FormFragment.java
@@ -3,10 +3,12 @@ package cat.tecnocampus.mobileapps.practicafinal.DavidJimenezBelmonte.AlbertSaen
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class FormFragment extends Fragment {
 
@@ -15,6 +17,17 @@ public class FormFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_form, container, false);
+
+        Button submitBtn = rootView.findViewById(R.id.SubmitBtn);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentContainer, new ResultFragment());
+                fragmentTransaction.commit();
+            }
+        });
+
         return rootView;
     }
 }
