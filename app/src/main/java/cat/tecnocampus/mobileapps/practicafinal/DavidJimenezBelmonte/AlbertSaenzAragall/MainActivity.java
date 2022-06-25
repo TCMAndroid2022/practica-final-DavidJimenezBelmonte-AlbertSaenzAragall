996a8507f5c5c844bf24cd1478ab34896be47f2b
David Jimenez Belmonte <d.jimenez.belmonte@gmail.com>
2022-06-25 12:44:50 +0200

    cosos de api

--- app/src/main/java/cat/tecnocampus/mobileapps/practicafinal/DavidJimenezBelmonte/AlbertSaenzAragall/MainActivity.java
+++ app/src/main/java/cat/tecnocampus/mobileapps/practicafinal/DavidJimenezBelmonte/AlbertSaenzAragall/MainActivity.java
@@ -3,6 +3,7 @@ package cat.tecnocampus.mobileapps.practicafinal.DavidJimenezBelmonte.AlbertSaen
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView firebaseTest = findViewById(R.id.firebase_test);
+
     }
 }
